fc27598f2d135e6a06aa422aa44eb3a296b73a26
MHShihan <mahmodul.no.1@gmail.com>
2023-09-16 22:47:25 +0600

    total credit time and total remaining time added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,13 +1,20 @@
-const Cart = ({ selectedCourse }) => {
+const Cart = ({ selectedCourse, totalTime, timeLeft }) => {
   //   const { course_name, id } = selectedCourse;
   //   console.log(selectedCourse);
   return (
-    <div>
-      <h3>I am cart: {selectedCourse.length}</h3>
+    <div className="bg-white p-6 rounded-lg">
+      <h3 className="text-l font-bold text-[#2F80ED] mb-2">
+        Credit Hour Remaining {timeLeft} hr
+      </h3>
+      <hr />
+      <h2 className="text-xl font-bold mt-2 mb-2">Course Name</h2>
 
       {selectedCourse.map((course) => (
         <li key={course.id}>{course.course_name}</li>
       ))}
+
+      <hr className="mt-2 mb-2" />
+      <h2>Total Credit Hour : {totalTime}</h2>
     </div>
   );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,8 @@ import Cart from "../cart/Cart";
 const Home = () => {
   const [courses, setCourses] = useState([]);
   const [selectedCourse, setSelectedCourse] = useState([]);
+  const [totalTime, setTotalTime] = useState([]);
+  const [timeLeft, setTimeLeft] = useState([]);
 
   useEffect(() => {
     fetch("../../../public/data.json")
@@ -14,12 +16,24 @@ const Home = () => {
 
   const handleSelectCourse = (course) => {
     const isExist = selectedCourse.find((item) => item.id === course.id);
+
+    let count = parseInt(course.credit);
     if (isExist) {
-      alert("This Course is already selected");
+      return alert("This Course is already selected");
     } else {
-      setSelectedCourse([...selectedCourse, course]);
+      selectedCourse.forEach((item) => {
+        count = count + parseInt(item.credit);
+      });
+
+      const remainingTime = 20 - count;
+      if (count > 20) {
+        return alert("You can watch only 20hr");
+      } else {
+        setTotalTime(count);
+        setTimeLeft(remainingTime);
+        setSelectedCourse([...selectedCourse, course]);
+      }
     }
-    console.log(isExist);
   };
   console.log(selectedCourse);
 
@@ -28,10 +42,9 @@ const Home = () => {
       <h1 className="text-5xl text-center font-bold py-10">
         Course Registration
       </h1>
-      <h2>Length:{courses.length} </h2>
 
-      <div className="flex flex-1">
-        <div className=" mx-auto grid grid-cols-3 gap-10">
+      <div className="container mx-auto flex gap-20 ">
+        <div className="  grid grid-cols-3 gap-10">
           {courses.map((course) => (
             <div key={course.id}>
               <div className="border-gray-200 border-2 rounded-lg p-2 w-80  ">
@@ -67,8 +80,11 @@ const Home = () => {
           ))}
         </div>
         <div>
-          <h1 className="text-3xl font-bold text-center mr-10">Add Course</h1>
-          <Cart selectedCourse={selectedCourse}></Cart>
+          <Cart
+            selectedCourse={selectedCourse}
+            totalTime={totalTime}
+            timeLeft={timeLeft}
+          ></Cart>
         </div>
       </div>
     </div>
